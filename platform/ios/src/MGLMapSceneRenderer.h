208dc7e8c78ca0945b5d9728eaa8e221f1878e9e
Teemu Harju <teemu.harju@gmail.com>
2017-02-16 07:25:32 +0200

    Test app for SceneKit and stub scene renderer for map.

---
+++ platform/ios/src/MGLMapSceneRenderer.h
@@ -0,0 +1,13 @@
+//
+//  MGLMapSceneRenderer.h
+//  ios
+//
+//  Created by Teemu Harju on 11/02/2017.
+//  Copyright © 2017 Mapbox. All rights reserved.
+//
+
+#ifndef MGLMapSceneRenderer_h
+#define MGLMapSceneRenderer_h
+
+
+#endif /* MGLMapSceneRenderer_h */
---
+++ platform/ios/src/MGLMapSceneRenderer.mm
@@ -0,0 +1,9 @@
+//
+//  MGLMapSceneRenderer.m
+//  ios
+//
+//  Created by Teemu Harju on 11/02/2017.
+//  Copyright © 2017 Mapbox. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
